2f03707e0fb0ce3d4db9b3c0d15949f97f75c98b
Van Nhan <nguyenvannhan0810@gmail.com>
2020-04-18 10:05:19 +0700

    Laravel Movie App - UI Interactivity w/ Alpine.js - Part 5

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,6 +9,9 @@
     <title>Movie App</title>
 
     <link rel="stylesheet" href="/css/main.css">
+
+    <livewire:styles />
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 </head>
 <body class="font-sans bg-gray-900 text-white">
 <nav class="border-b border-gray-800">
@@ -35,17 +38,7 @@
         </ul>
 
         <div class="flex flex-col md:flex-row items-center">
-            <div class="relative mt-3 md:mt-0">
-                <input type="text"
-                       class="bg-gray-800 rounded-full w-64 px-4 py-1 pl-8 text-sm focus:outline-none focus:shadow-outline"
-                       name="" id="" placeholder="Search">
-                <div class="absolute top-0">
-                    <svg class="fill-current w-4 text-gray-500 mt-2 ml-2" viewBox="0 0 24 24">
-                        <path class="heroicon-ui"
-                              d="M16.32 14.9l5.39 5.4a1 1 0 01-1.42 1.4l-5.38-5.38a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z"/>
-                    </svg>
-                </div>
-            </div>
+            <livewire:search-dropdown />
 
             <div class="md:ml-4 mt-3 md:mt-0">
                 <a href="#">
@@ -57,6 +50,8 @@
 </nav>
 
 @yield('content')
+
+<livewire:scripts />
 
 </body>
 </html>
--- tests/Feature/ViewMoviesTest.php
+++ tests/Feature/ViewMoviesTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Http;
+use Livewire\Livewire;
 use Tests\TestCase;
 
 class ViewMoviesTest extends TestCase
@@ -43,6 +44,50 @@ class ViewMoviesTest extends TestCase
         $response->assertSee('Jeanne McCarthy');
         $response->assertSee('Casting Director');
         $response->assertSee('Dwayne Johnson');
+    }
+
+    /**
+     * @test
+     */
+    public function the_search_dropdown_works_correctly() {
+        Http::fake([
+            'https://api.themoviedb.org/3/search/movie?query=jumanji' => $this->fakeSearchMovies(),
+        ]);
+
+        Livewire::test('search-dropdown')
+            ->assertDontSee('jumanji')
+            ->set('search', 'jumanji')
+            ->assertSee('Jumanji');
+    }
+
+    private function fakeSearchMovies()
+    {
+        return Http::response([
+            'results' => [
+                [
+                    "popularity" => 406.677,
+                    "vote_count" => 2607,
+                    "video" => false,
+                    "poster_path" => "/xBHvZcjRiWyobQ9kxBhO6B2dtRI.jpg",
+                    "id" => 419704,
+                    "adult" => false,
+                    "backdrop_path" => "/5BwqwxMEjeFtdknRV792Svo0K1v.jpg",
+                    "original_language" => "en",
+                    "original_title" => "Fake Movie",
+                    "genre_ids" => [
+                        12,
+                        18,
+                        9648,
+                        878,
+                        53,
+                    ],
+                    "title" => "Jumanji",
+                    "vote_average" => 6,
+                    "overview" => "Fake movie description. The near future, a time when both hope and hardships drive humanity to look to the stars and beyond. While a mysterious phenomenon menaces to destroy life on planet earth.",
+                    "release_date" => "2019-09-17",
+                ]
+            ]
+        ], 200);
     }
 
     private function fakePopularMovies()
