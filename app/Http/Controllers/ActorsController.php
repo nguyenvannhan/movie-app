6f36f1de4ff4f360f46e08977db560dfb802ad8f
Van Nhan <nguyenvannhan0810@gmail.com>
2020-04-19 15:46:33 +0700

    Finish Part 7

--- app/Http/Controllers/ActorsController.php
+++ app/Http/Controllers/ActorsController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\ViewModels\ActorsViewModel;
+use App\ViewModels\ActorViewModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class ActorsController extends Controller
 {
@@ -11,9 +14,16 @@ class ActorsController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index($page = 1)
     {
-        return view('actors.index');
+        abort_if($page > 500, 204);
+        $popularActors = Http::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/person/popular?page='.$page)
+            ->json()['results'];
+
+        $viewModel = new ActorsViewModel($popularActors, $page);
+
+        return view('actors.index', $viewModel);
     }
 
     /**
@@ -45,7 +55,21 @@ class ActorsController extends Controller
      */
     public function show($id)
     {
-        //
+        $actor = Http::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/person/'.$id)
+            ->json();
+
+        $social = Http::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/person/'.$id.'/external_ids')
+            ->json();
+
+        $credits = Http::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/person/'.$id.'/combined_credits')
+            ->json();
+
+        $viewModel = new ActorViewModel($actor, $social, $credits);
+
+        return view('actors.show', $viewModel);
     }
 
     /**
